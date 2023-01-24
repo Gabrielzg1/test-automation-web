129924df025518b56b6ae6edaa2023da64e6f61c
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-01-23 23:08:29 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "http://localhost:2020",
+	baseURL: "https://test-automation-server.onrender.com",
 });
 
 export const createAdminSession = async (email, password) => {
